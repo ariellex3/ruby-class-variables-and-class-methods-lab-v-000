d9c883ad12d1e193c4b7bea9ed85315346612890
Arielle D <domingu4@gmail.com>
2018-11-16 20:43:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  def @@genres
-    @@genres = []
-    @@genres = genre.unique
-  end
+  @@genres = []
 
 
   def initialize(name,artist,genre)
